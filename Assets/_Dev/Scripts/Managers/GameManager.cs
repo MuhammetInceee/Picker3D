216cbeb2db6ad6ceb4f1daa007f14a658dae1092
Muhammet İNCE <muhammetinceee@gmail.com>
2023-11-20 02:51:17 +0300

    Singleton Added

---
+++ Assets/_Dev/Scripts/Managers/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+namespace Picker.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_Dev/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Picker.Utilities
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static volatile T instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+
+                if (instance == null)
+                {
+                    instance = FindObjectOfType(typeof(T)) as T;
+                }
+
+                return instance;
+            }
+            set { instance = value; }
+        }
+    }
+}
